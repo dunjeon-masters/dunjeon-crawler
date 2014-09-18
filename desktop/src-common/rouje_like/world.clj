718d916df21b0c6eb2014ff9b737f56705ebb25a
Anthony <anthony.dayzerostudio@gmail.com>
2014-09-17 23:52:19 -0700

    Created memorized version of the textures map

--- desktop/src-common/rouje_like/world.clj
+++ desktop/src-common/rouje_like/world.clj
@@ -1,13 +1,17 @@
 (ns rouje-like.world
-  (:import [com.badlogic.gdx.graphics.g2d SpriteBatch])
+  (:import [com.badlogic.gdx.graphics.g2d SpriteBatch]
+           [clojure.lang Keyword])
 
   (:require [play-clj.g2d :refer :all]
 
             [rouje-like.components :as rj.c]
             [rouje-like.entity     :as rj.e]))
 
-(def block-size 27)
-(def world-sizes [20 20])
+(def get-texture (memoize
+                   (fn [^Keyword type]
+                     (type (zipmap [:player :wall :empty :gold]
+                                   (map (comp texture* (fn [s] (str s ".jpg")))
+                                        ["at" "percent" "period" "dollar"]))))))
 
 (defn new-tile
   [x y & args]
@@ -31,55 +35,48 @@
       world)))
 
 (defn generate-random-world
-  ([] (generate-random-world world-sizes))
-
-  ([[width height]]
-   ;; GENERATE-SKELETON
-   (let [board (atom (vec
-                          (map vec
-                               (for [x (range width)]
-                                 (for [y (range height)]
-                                   (new-tile (* x block-size)
-                                             (* y block-size)
-                                             :type :wall))))))]
-     ;; GENERATE-BODY
-     (doseq [x (range 1 (dec width))
-             y (range 1 (dec height))
-             :let [tile (new-tile (* x block-size)
-                                  (* y block-size)
-                                  :type :empty)]]
-       (swap! board (fn [old]
-                         (update-in old [x y]
-                                    (fn [_] tile)))))
-     ;; GENERATE-TREASURE
-     (swap! board (fn [old]
-                    (init-treasure old 50)))
-     ;; ADD-PLAYER, hardcoded at [10, 10]
-     (swap! board (fn [old]
-                    (update-in old [10 10]
-                               (fn [_] (new-tile (* 10 block-size)
-                                                 (* 10 block-size)
-                                                 :type :player)))))
-     board)))
+  [block-size [width height]]
+  ;; GENERATE-SKELETON
+  (let [board (atom (vec
+                      (map vec
+                           (for [x (range width)]
+                             (for [y (range height)]
+                               (new-tile (* x block-size)
+                                         (* y block-size)
+                                         :type :wall))))))]
+    ;; GENERATE-BODY
+    (doseq [x (range 1 (dec width))
+            y (range 1 (dec height))
+            :let [tile (new-tile (* x block-size)
+                                 (* y block-size)
+                                 :type :empty)]]
+      (swap! board (fn [old]
+                     (update-in old [x y]
+                                (fn [_] tile)))))
+    ;; GENERATE-TREASURE
+    (swap! board (fn [old]
+                   (init-treasure old 50)))
+    ;; ADD-PLAYER, hardcoded at [10, 10]
+    (swap! board (fn [old]
+                   (update-in old [10 10]
+                              (fn [_] (new-tile (* 10 block-size)
+                                                (* 10 block-size)
+                                                :type :player)))))
+    board))
 
 (defn render-world!
   [system entity]
   #_(println "rj.wr/render-world!")
   (let [renderer (new SpriteBatch)
         c-world (rj.e/get-c system entity :world)
-        board @(:tiles c-world)
-        textures {:player (texture "at.jpg")
-                  :wall   (texture "percent.jpg")
-                  :empty  (texture "period.jpg")
-                  :gold   (texture "dollar.jpg")}]
+        board @(:tiles c-world)]
     (.begin renderer)
     (doseq [col board
             row col]
-      (let [texture-region (:object ((:type row) textures))]
+      (let [texture-region (:object (get-texture (:type row)))]
         (.draw renderer
                texture-region
                (float (:x row))
                (float (:y row)))))
-    (.end renderer))
-  system)
+    (.end renderer)))
 
