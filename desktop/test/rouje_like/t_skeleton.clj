2858d75c591b70c3313804ad4427e63441dbe2e5
wc_karatekid <rtanase@ucsc.edu>
2014-12-10 23:46:28 -0800

    snake functionality not working. made a bunch of mob files to be filled in

---
+++ desktop/test/rouje_like/t_skeleton.clj
@@ -0,0 +1,17 @@
+(ns rouje-like.t-skeleton
+  (:use [midje.sweet]
+        [rouje-like.skeleton])
+  (:require [rouje-like.core :as rj.core]
+            [brute.entity :as br.e]
+            [rouje-like.entity-wrapper :as rj.e]
+            [rouje-like.utils :refer :all]
+            [rouje-like.components :as rj.c :refer [->3DPoint]]
+            [rouje-like.world :as rj.w]))
+
+(defn get-system []
+  (with-open [w (clojure.java.io/writer "NUL")]
+    (binding [*out* w]
+      (-> (br.e/create-system)
+          (rj.core/init-entities {})))))
+
+(fact "add-skeleton")
