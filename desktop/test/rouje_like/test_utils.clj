3bf53f3ef62ad4a44a435ac69433984d4b889bf6
pancia <anthony.dayzerostudio@gmail.com>
2014-12-16 04:34:37 -0800

    Added dev-null as a way to write to a null device regardless of OS

--- desktop/test/rouje_like/test_utils.clj
+++ desktop/test/rouje_like/test_utils.clj
@@ -3,19 +3,26 @@
   (:require [brute.entity :as br.e]
             [rouje-like.core :as rj.core]))
 
+(def dev-null
+  (let [os (System/getProperty "os.name")]
+    (case os
+      "Mac OS X" "/dev/null"
+      "Linux" "/dev/null"
+       "NUL")))
+
 (defn init []
-  (with-open [w (clojure.java.io/writer "NUL")]
+  (with-open [w (clojure.java.io/writer dev-null)]
     (binding [*out* w]
       (br.e/create-system))))
 
 (defn start
   ([]
-   (with-open [w (clojure.java.io/writer "NUL")]
+   (with-open [w (clojure.java.io/writer dev-null)]
      (binding [*out* w]
        (-> (init)
            (rj.core/init-entities {})))))
   ([system]
-   (with-open [w (clojure.java.io/writer "NUL")]
+   (with-open [w (clojure.java.io/writer dev-null)]
      (binding [*out* w]
        (-> system
            (rj.core/init-entities {})))))
@@ -26,7 +33,7 @@
            (rj.core/init-entities {}))))))
 
 (fact "init"
-      (init) => {:entity-component-types {},
+      (init) => {:entity-component-types {}
                  :entity-components      {}})
 
 (fact "start"
