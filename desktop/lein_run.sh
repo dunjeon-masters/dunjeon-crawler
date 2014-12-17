9a180b748bbd860482dc3b9f42d25d3624e031a1
pancia <anthony.dayzerostudio@gmail.com>
2014-12-17 00:25:33 -0800

    lein_run.sh now clears the screen with ='s

--- desktop/lein_run.sh
+++ desktop/lein_run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+function my_clear {
+    for i in `seq 1 42`;
+    do
+        for j in `seq 1 80`;
+        do
+            printf "="
+        done
+        printf "\n"
+    done
+}
+
 clear
+my_clear
+lein clean
 lein run 2> >(grep -P "(\tat rouje)|Caused|Exception")
