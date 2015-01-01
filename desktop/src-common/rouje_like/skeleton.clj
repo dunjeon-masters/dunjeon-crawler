199e6de7dcea6ddd623002e828928a7cff29db68
Pancia <anthony.dayzerostudio@gmail.com>
2015-01-01 01:58:59 -0800

    Testing out (new) spawnable.clj in skeleton.
    Using spawnable to contain the boilerplate logic for getting a tile to
    put the entity, and the code to insert it into the world.
    
    The plan is to add a defmonster macro that will reduce the boilerplate
    even further.

--- desktop/src-common/rouje_like/skeleton.clj
+++ desktop/src-common/rouje_like/skeleton.clj
@@ -1,76 +1,55 @@
 (ns rouje-like.skeleton
-  (:require [brute.entity :as br.e]
-
-            [rouje-like.entity-wrapper :as rj.e]
+  (:require [rouje-like.entity-wrapper :as rj.e]
             [rouje-like.utils :as rj.u :refer [?]]
             [rouje-like.tickable :as rj.t]
-            [rouje-like.components :as rj.c :refer [can-move? move
-                                                    can-attack? attack
-                                                    ->3DPoint ->2DPoint]]
+            [rouje-like.components :as rj.c
+             :refer [can-move? move
+                     can-attack? attack
+                     ->3DPoint ->2DPoint]]
             [rouje-like.mobile :as rj.m]
-            [rouje-like.messaging :as rj.msg]
             [rouje-like.destructible :as rj.d]
-            [rouje-like.utils :as rj.u]
             [rouje-like.attacker :as rj.atk]
-            [rouje-like.status-effects :as rj.stef]
             [rouje-like.config :as rj.cfg]
+            [rouje-like.spawnable :as rj.spawn]
+
             [clojure.set :refer [union]]))
 
 #_(use 'rouje-like.skeleton :reload)
 
 (defn add-skeleton
-  ([{:keys [system z]}]
-   (let [e-world (first (rj.e/all-e-with-c system :world))
-         c-world (rj.e/get-c-on-e system e-world :world)
-         levels (:levels c-world)
-         world (nth levels z)
-
-         get-rand-tile (fn [world]
-                         (get-in world [(rand-int (count world))
-                                        (rand-int (count (first world)))]))]
-     (loop [target-tile (get-rand-tile world)]
-       (if (rj.cfg/<floors> (:type (rj.u/tile->top-entity target-tile)))
-         (add-skeleton system target-tile)
-         (recur (get-rand-tile world))))))
-  ([system target-tile]
-   (let [e-world (first (rj.e/all-e-with-c system :world))
-         e-skeleton (br.e/create-entity)
-         hp (:hp (rj.cfg/entity->stats :skeleton))
-         system (rj.u/update-in-world system e-world
-                                      [(:z target-tile) (:x target-tile) (:y target-tile)]
-                                      (fn [entities]
-                                        (vec
-                                          (conj
-                                            (remove #(#{:wall} (:type %)) entities)
-                                            (rj.c/map->Entity {:id   e-skeleton
-                                                               :type :skeleton})))))]
-     {:system (rj.e/system<<components
-                system e-skeleton
-                [[:skeleton {}]
-                 [:position {:x    (:x target-tile)
-                             :y    (:y target-tile)
-                             :z    (:z target-tile)
-                             :type :skeleton}]
-                 [:mobile {:can-move?-fn (fn [c-mobile e-this t-tile system]
-                                           (rj.m/can-move? c-mobile e-this t-tile system))
-                           :move-fn      rj.m/move}]
-                 [:sight {:distance 4}]
-                 [:attacker {:atk              (:atk (rj.cfg/entity->stats :skeleton))
-                             :can-attack?-fn   rj.atk/can-attack?
-                             :attack-fn        rj.atk/attack
-                             :status-effects   []
-                             :is-valid-target? (partial #{:player})}]
-                 [:destructible {:hp             hp
-                                 :max-hp         hp
-                                 :def            (:def (rj.cfg/entity->stats :skeleton))
-                                 :can-retaliate? false
-                                 :take-damage-fn rj.d/take-damage
-                                 :on-death-fn    nil
-                                 :status-effects []}]
-                 [:killable {:experience (:exp (rj.cfg/entity->stats :skeleton))}]
-                 [:energy {:energy 1
-                           :default-energy 1}]
-                 [:tickable {:tick-fn rj.t/process-input-tick
-                             :pri 0}]
-                 [:broadcaster {:name-fn (constantly "the skeleton")}]])
-      :z (:z target-tile)})))
+  [{:keys [system z]}]
+  (let [tile (rj.spawn/get-tile system z)
+        e-skeleton (rj.spawn/new-entity)
+        type-e :skeleton]
+    (->>
+      (rj.e/system<<components
+        system e-skeleton
+        [[:skeleton     {}]
+         [:position     {:x    (:x tile)
+                         :y    (:y tile)
+                         :z    (:z tile)
+                         :type type-e}]
+         [:mobile       {:can-move?-fn (fn [c-mobile e-this t-tile system]
+                                         (rj.m/can-move? c-mobile e-this t-tile system))
+                         :move-fn rj.m/move}]
+         [:sight        {:distance 4}]
+         [:attacker     {:atk              (:atk (rj.cfg/entity->stats type-e))
+                         :can-attack?-fn   rj.atk/can-attack?
+                         :attack-fn        rj.atk/attack
+                         :status-effects   []
+                         :is-valid-target? (partial #{:player})}]
+         [:destructible {:hp             (:hp  (rj.cfg/entity->stats type-e))
+                         :max-hp         (:hp  (rj.cfg/entity->stats type-e))
+                         :def            (:def (rj.cfg/entity->stats type-e))
+                         :can-retaliate? false
+                         :take-damage-fn rj.d/take-damage
+                         :on-death-fn    nil
+                         :status-effects []}]
+         [:killable     {:experience (:exp (rj.cfg/entity->stats type-e))}]
+         [:energy       {:energy 1
+                         :default-energy 1}]
+         [:tickable     {:tick-fn rj.t/process-input-tick
+                         :pri 0}]
+         [:broadcaster  {:name-fn (constantly "the skeleton")}]])
+      (rj.spawn/put-in-world type-e tile e-skeleton z)
+      (assoc {} :z z :system))))
---
+++ desktop/src-common/rouje_like/spawnable.clj
@@ -0,0 +1,35 @@
+(ns rouje-like.spawnable
+  (:require [rouje-like.utils :as rj.u]
+            [brute.entity :as br.e]
+            [rouje-like.entity-wrapper :as rj.e]
+            [rouje-like.config :as rj.cfg]
+            [rouje-like.components :as rj.c
+             :refer [->3DPoint]]))
+
+(defn get-tile
+  [system z]
+  (let [e-world (first (rj.e/all-e-with-c system :world))
+        {:keys [levels]} (rj.e/get-c-on-e system e-world :world)
+        world (nth levels z)
+        get-rand-tile (fn [world]
+                        (get-in world [(rand-int (count world))
+                                       (rand-int (count (first world)))]))]
+    (loop [target-tile (get-rand-tile world)]
+      (if (rj.cfg/<floors> (:type (rj.u/tile->top-entity target-tile)))
+        target-tile
+        (recur (get-rand-tile world))))))
+
+(defn new-entity []
+  (br.e/create-entity))
+
+(defn put-in-world [type-e tile entity z system]
+  (let [e-world (first (rj.e/all-e-with-c system :world))]
+    (rj.u/update-in-world
+      system e-world
+      (->3DPoint tile)
+      (fn [entities]
+        (vec
+          (conj
+            (remove #(#{:wall} (:type %)) entities)
+            (rj.c/map->Entity {:id   entity
+                               :type type-e})))))))
