b96b4da3e51414d5d8d39e35b6505b676cb16d9b
Pancia <anthony.dayzerostudio@gmail.com>
2015-01-01 19:48:35 -0800

    Necromancer now using tickable.clj

--- desktop/src-common/rouje_like/necromancer.clj
+++ desktop/src-common/rouje_like/necromancer.clj
@@ -9,6 +9,7 @@
             [rouje-like.destructible :as rj.d]
             [rouje-like.attacker :as rj.atk]
             [rouje-like.config :as rj.cfg]
+            [rouje-like.tickable :as rj.t]
             [rouje-like.status-effects :as rj.stef]))
 
 (declare process-input-tick)
@@ -64,49 +65,7 @@
                                  :on-death-fn nil
                                  :take-damage-fn rj.d/take-damage}]
                  [:killable {:experience (:exp (rj.cfg/entity->stats :necro))}]
-                 [:tickable {:tick-fn process-input-tick
+                 [:tickable {:tick-fn rj.t/process-input-tick
                              :pri 0}]
                  [:broadcaster {:name-fn (constantly "the necromancer")}]])
       :z (:z target-tile)})))
-
-(defn process-input-tick
-  [_ e-this system]
-  (let [c-position (rj.e/get-c-on-e system e-this :position)
-        this-pos [(:x c-position) (:y c-position)]
-        c-mobile (rj.e/get-c-on-e system e-this :mobile)
-
-        e-player (first (rj.e/all-e-with-c system :player))
-        c-player-pos (rj.e/get-c-on-e system e-player :position)
-        player-pos [(:x c-player-pos) (:y c-player-pos)]
-
-        e-world (first (rj.e/all-e-with-c system :world))
-        c-world (rj.e/get-c-on-e system e-world :world)
-        levels (:levels c-world)
-        level (nth levels (:z c-position))
-
-        neighbor-tiles (rj.u/get-neighbors level [(:x c-position) (:y c-position)])
-
-        c-sight (rj.e/get-c-on-e system e-this :sight)
-        is-player-within-range? (seq (rj.u/get-neighbors-of-type-within level this-pos [:player]
-                                                                        #(<= %  (:distance c-sight))))
-
-        c-attacker (rj.e/get-c-on-e system e-this :attacker)
-
-        target-tile (if (and (rj.u/can-see? level (:distance c-sight) this-pos player-pos)
-                             is-player-within-range?)
-                      (rj.u/get-closest-tile-to level this-pos (first is-player-within-range?))
-                      (if (seq neighbor-tiles)
-                        (rand-nth (conj neighbor-tiles nil))
-                        nil))
-        e-target (:id (rj.u/tile->top-entity target-tile))]
-    (if (not (nil? target-tile))
-      (cond
-        (and (< (rand-int 100) 80)
-             (can-move? c-mobile e-this target-tile system))
-        (move c-mobile e-this target-tile system)
-
-        (can-attack? c-attacker e-this e-target system)
-        (attack c-attacker e-this e-target system)
-
-        :else system)
-      system)))
